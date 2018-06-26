61022ee5eb1be791eff4b9fdbe4ae1fe6268a6d7
chisom dike <dike4mii4real@gmail.com>
2018-06-26 08:40:58 +0100

    prolie page

--- src/components/scenes/Profile.js
+++ src/components/scenes/Profile.js
@@ -20,9 +20,29 @@ export default class Profile extends Component {
                 <Text style={{ fontWeight: '600', flex: 1, textAlign: 'center', fontSize: 18, fontFamily: "ProximaNova-Semibold" }} > Chisom Dike </Text>
                     <Icon size={26} name='brightness-low' onPress={this.addPerson} color="black"  />  
                 </View>
-                <View style={{ height: width/2, width }}> 
-                    <View style={{ width: '40%', }}>
-                            <Image source={{img}} style={{ width: 70, height: 70, borderRadius: 70 }} />
+                <View style={{ height: width/2, width, marginTop: 10 , marginLeft: 10, flexDirection: 'row'}}> 
+                    <View style={{ width: '30%', }}>
+                            <Image source={img} style={{ width: 90, height: 90, borderRadius: 90 }} />
+                            <Text> Chisom Dike </Text>
+                    </View>
+                    <View style={{ width: '60%',marginTop: 10 }}>
+                        <View style={{ marginHorizontal: 10, flexDirection: 'row', justifyContent: 'space-between' }}> 
+                            <View> 
+                                <Text> 3160</Text>
+                                <Text> Posts </Text>
+                            </View>
+                            <View> 
+                                <Text> 3160</Text>
+                                <Text> Followers </Text>
+                            </View>
+                            <View> 
+                                <Text> 3160</Text>
+                                <Text> Following </Text>
+                            </View>
+                        </View> 
+                        <TouchableOpacity activeOpacity={0.7} style={{ backgroundColor: '#c9c9c9', marginTop:10, height: 33, justifyContent: 'center', alignItems: 'center' }}>
+                            <Text> Edit Profile </Text>
+                        </TouchableOpacity>    
                     </View>
                 </View>
 
